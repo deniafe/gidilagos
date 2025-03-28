d776b1c389268ac110f8bac0113da8af649a580d
deniafe <detoyin@yahoo.com>
2025-03-28 04:46:55 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,33 @@
 import type { Metadata } from "next";
-import { Montserrat_Alternates } from 'next/font/google'
+import { Montserrat_Alternates } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { siteConfig } from "@/config/site";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, RedirectToSignIn } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
 import EventProvider from "@/providers/event-provider";
 import ModalProvider from "@/providers/modal-provider";
+import { redirect } from "next/navigation";
 
 const expletus_sans = Montserrat_Alternates({
-  weight: ['400', '500'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  display: 'swap',
-})
+  weight: ["400", "500"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
-  title:{
+  title: {
     default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`
+    template: `%s | ${siteConfig.name}`,
   },
-  description: siteConfig.description, 
+  description: siteConfig.description,
   icons: [
     {
-      url: '/favicon.svg',
-      href: '/favicon.svg'
-    }
-  ]
+      url: "/favicon.svg",
+      href: "/favicon.svg",
+    },
+  ],
 };
 
 export default function RootLayout({
@@ -34,25 +35,23 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // Redirect all visitors to sign-in page
+  redirect("/sign-in"); 
+
   return (
-    <html lang="en">
-      <body className={expletus_sans.className}>
-      <ClerkProvider >
-        <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-            >
-          <ModalProvider>
-            <EventProvider>
-              {children}
-            </EventProvider>
-           </ModalProvider>
-          <Toaster />
-        </ThemeProvider>
-      </ClerkProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={expletus_sans.className}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <ModalProvider>
+              <EventProvider>
+                {children}
+              </EventProvider>
+            </ModalProvider>
+            <Toaster />
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
