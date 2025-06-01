55e74aa76f269676925543de24d4b9dda8d23a45
deniafe <deniafe@gmail.com>
2025-06-01 07:22:52 +0100

    removed uneccesary imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,10 @@ import { Montserrat_Alternates } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { siteConfig } from "@/config/site";
-import { ClerkProvider, RedirectToSignIn } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
 import EventProvider from "@/providers/event-provider";
 import ModalProvider from "@/providers/modal-provider";
-import { redirect } from "next/navigation";
 
 const expletus_sans = Montserrat_Alternates({
   weight: ["400", "500"],
