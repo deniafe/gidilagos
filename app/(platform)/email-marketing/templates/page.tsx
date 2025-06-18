cb5b351bc8b7ff3903a5329aefaf7598bffce069
deniafe <deniafe@gmail.com>
2025-06-18 03:26:46 +0100

    Added email template page

---
+++ app/(platform)/email-marketing/templates/page.tsx
@@ -0,0 +1,12 @@
+'use client'; // Add this if you're using client-side features
+
+import React from 'react';
+
+export default function TemplatesPage() {
+  return (
+    <div>
+      <h1>Email Templates</h1>
+      {/* Your page content */}
+    </div>
+  );
+}
